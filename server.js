1bd1c2090a37e4c16785b208043198d944d0d725
Tony Fasube <fasubeblessing@gmail.com>
2025-09-10 12:10:30 +0100

    2nd Commit

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
+require('dotenv').config();
+
 const express = require('express');
 const PORT = 8080;
 const app = express();
+const mongoose = require('mongoose');
+const db = process.env.DATABASE;
 
 
 
+mongoose.connect(db).then(()=> {
+    console.log('The database connection has been established successfully');
 
-app.listen(PORT, ()=>{
+    app.listen(PORT, ()=>{
     console.log(`Server is running on the PORT: ${PORT}`);
+})
+}).catch((error) => {
+    console.log('Error connecting to the database', error.message);
     
 })
