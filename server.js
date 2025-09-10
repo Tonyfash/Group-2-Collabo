a2179b238db19cef61c8c1458563f060097bb046
Tony Fasube <fasubeblessing@gmail.com>
2025-09-10 10:30:58 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const PORT = 8080;
+const app = express();
+
+
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on the PORT: ${PORT}`);
+    
+})
